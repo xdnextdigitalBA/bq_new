23af603f6a8158fe4844bd2f6a09ca84f64df368
Benedict Altgassen <benedict.altgassen@nextdigital.de>
2024-01-25 09:56:56 +0100

    Inkrementelle Aktualisierung ergänzt, um Query-Größe zu optimieren

--- models/staging/google_analytics/NEW/stg_google_analytics_283762334.sql
+++ models/staging/google_analytics/NEW/stg_google_analytics_283762334.sql
@@ -1,6 +1,27 @@
+{{
+  config(
+    materialized = 'incremental',
+    partition_by = {
+      'field': 'event_date', 
+      'data_type': 'date', 
+      'granularity': 'day'},
+    incremental_strategy = 'insert_overwrite',
+    unique_key='event_id',
+    on_schema_change = 'fail',
+    tags=['incremental']
+  )
+}}
+
 --NEW-Wärme
 WITH _source AS(
   SELECT
+    -- generate surrogate 'event' ID
+        {{ dbt_utils.generate_surrogate_key([
+           'event_timestamp',
+           'event_name',
+           'user_pseudo_id',
+           'ARRAY_TO_STRING(ARRAY(SELECT CONCAT(p.key, "::", COALESCE(p.value.string_value, CAST(p.value.int_value AS STRING), CAST(p.value.float_value AS STRING), CAST(p.value.double_value AS STRING))) FROM UNNEST(event_params) AS p), "; ")'
+        ]) }} AS event_id,
     PARSE_DATE('%Y%m%d', event_date) as event_date,
     TIMESTAMP_MICROS(event_timestamp) as event_ts,
     
@@ -120,6 +141,19 @@ WITH _source AS(
   FROM {{ source('analytics_283762334', 'events')}} as events
   LEFT JOIN UNNEST (event_params) as params
   LEFT JOIN UNNEST(items) as items 
+{% if is_incremental() %}
+     WHERE
+           -- events from the 'intraday' tables should always be included
+           _table_suffix LIKE 'intraday_%'
+        OR (
+           -- add/replace data from last daily partition onwards, regardless:
+              PARSE_DATE('%Y%m%d', _table_suffix) >= DATE(_dbt_max_partition)
+           -- add/replace data originally generated
+           -- today, yesterday, or the day-before-yesterday
+           -- (events_intraday data can be mutated on transfer to events)
+           OR PARSE_DATE('%Y%m%d', _table_suffix) >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
+           )
+{% endif %}  
 ),
 
 events_aggregated as (
